80fa05dfff82f7ecb8bdd62a6b97830705512f88
Filipe Ximenes <filipeximenes@gmail.com>
2016-04-21 12:28:13 -0300

    finishing first view tests

--- awesomebikes/bikes/tests/test_endpoints.py
+++ awesomebikes/bikes/tests/test_endpoints.py
@@ -4,7 +4,13 @@ from bikes.endpoints import BikeListView
 
 
 class BikeListTest(APITestCase):
+    view = BikeListView
+    view_name = 'bikes-list'
 
     def test_bike_list_url(self):
         resolver = resolve('/api/bikes/')
-        self.assertEqual(resolver.func.__name__, BikeListView.__name__)
+        self.assertEqual(resolver.func.__name__, self.view.__name__)
+
+    def test_bike_list_url_name(self):
+        resolver = resolve('/api/bikes/')
+        self.assertEqual(resolver.url_name, self.view_name)
