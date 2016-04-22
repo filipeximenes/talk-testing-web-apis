2b0b24c0596a94e24cf6286c85594978b6f035e2
Filipe Ximenes <filipeximenes@gmail.com>
2016-04-22 14:36:51 -0300

    adding request returns 200 test

--- awesomebikes/bikes/endpoints.py
+++ awesomebikes/bikes/endpoints.py
@@ -1,6 +1,10 @@
 from rest_framework.generics import ListAPIView
 from bikes.serializers import BikeSerializer
+from bikes.models import Bike
 
 
 class BikeListView(ListAPIView):
-	serializer_class = BikeSerializer
+    serializer_class = BikeSerializer
+
+    def get_queryset(self):
+        return Bike.objects.all()
--- awesomebikes/bikes/tests/test_endpoints.py
+++ awesomebikes/bikes/tests/test_endpoints.py
@@ -1,5 +1,6 @@
-from django.core.urlresolvers import resolve
+from django.core.urlresolvers import resolve, reverse
 from rest_framework.test import APITestCase
+from rest_framework import status
 from bikes.endpoints import BikeListView
 from bikes.serializers import BikeSerializer
 
@@ -16,6 +17,16 @@ class BikeListTest(APITestCase):
     def test_bike_list_url_name(self):
         resolver = resolve('/api/bikes/')
         self.assertEqual(resolver.url_name, self.view_name)
+
+    def test_bike_list_returns_200(self):
+        response = self.client.get(reverse(self.view_name))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_bike_list_returns_a_list(self):
+        response = self.client.get(reverse(self.view_name))
+
+        self.assertIsInstance(response.data, list)
 
     def test_bike_list_uses_bike_serializer(self):
         self.assertEqual(self.view_class().get_serializer_class(), BikeSerializer)
